ea754315189b44b4aade10a3c35f0ccfd63d5841
maazpatel24 <maazptl240602@gmail.com>
2022-10-31 14:23:22 +0530

    first commit

---
+++ react-social/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { AuthContextProvider } from "./context/AuthContext";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
